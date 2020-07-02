c2855faf237edab9e59b516890599199753a5551
Tyler McBride <tylerdb@Tylers-MacBook-Pro-2.local>
2020-07-01 23:36:54 -0700

    fix titles

---
+++ index.js
@@ -0,0 +1,60 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "employee_managementDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
+function mainMenu() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "Find songs by artist",
+        "Find all artists who appear more than once",
+        "Find data within a specific range",
+        "Search for a specific song",
+        "Find artists with a top song and top album in the same year"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Find songs by artist":
+        artistSearch();
+        break;
+
+      case "Find all artists who appear more than once":
+        multiSearch();
+        break;
+
+      case "Find data within a specific range":
+        rangeSearch();
+        break;
+
+      case "Search for a specific song":
+        songSearch();
+        break;
+
+      case "Find artists with a top song and top album in the same year":
+        songAndAlbumSearch();
+        break;
+      }
+    });
+}
